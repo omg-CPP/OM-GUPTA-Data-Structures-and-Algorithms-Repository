ddb994c09fb7e5bb18df5481e7ffe904eb9c894c
Om Gupta <omgbmw100@gmail.com>
2026-03-23 22:47:42 +0530

    First File Added

---
+++ R01_BASICS/L01_HelloWorld.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+// #include -> pre-processor directive
+// <iostream> -> Header file
+// cout and cin are built-in functions
+using namespace std;
+// std :: cout<<"Hello World";
+int main()
+{
+    cout<<"Hello World";
+    cout<<endl; // for next line
+    cout<<"Hello PW";
+    cout<<"\n"; // for next line
+    cout<<"Hello CW"<<endl;
+    cout<<"Hello GW";
+    return 0;
+}
+
+// Hello World
+// Hello PW
+// Hello CW
+// Hello GW
+
+// Compilation Process
+// MinGW -> Compiler
+// VS Code -> Text Editor provides functionality
+
+/* 
+   English like Language or High level Language (C++)
+   is converted or compiled into Machine level 
+   Language (0/1) or Low level Language 
+*/
