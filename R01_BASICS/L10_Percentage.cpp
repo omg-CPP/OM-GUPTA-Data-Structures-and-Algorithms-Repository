1dafab980dcd3a54837f136531020c400eda7e3f
Om Gupta <omgbmw100@gmail.com>
2026-03-24 02:30:37 +0530

    Day 2: file-added

---
+++ R01_BASICS/L10_Percentage.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float m1 = 90;
+    float m2 = 91;
+    float m3 = 92;
+    float m4 = 93;
+    float m5 = 94;
+    float percent = (m1+m2+m3+m4+m5)/5;
+    cout<<"The Percentage of Student is : "<<percent<<endl;
+    // The Percentage of Student is : 92
+    return 0;
+}
