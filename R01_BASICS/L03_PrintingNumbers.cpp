b0fd36280c6cec0988d682e901f35d72b36b1dcf
Om Gupta <omgbmw100@gmail.com>
2026-03-23 23:00:47 +0530

    new-file-added

---
+++ R01_BASICS/L03_PrintingNumbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<4<<endl;
+    cout<<4+3<<endl;
+    cout<<"4+3"<<endl;
+    cout<<9*7<<endl;
+    return 0;
+}
+
+// 4
+// 7
+// 4+3
+// 63
