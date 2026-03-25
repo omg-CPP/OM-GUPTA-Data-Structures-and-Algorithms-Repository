c726418a750c3e238ab520d96a9b279442ede436
Om Gupta <omgbmw100@gmail.com>
2026-03-25 21:20:52 +0530

    Day 3: file-added

---
+++ R01_BASICS/L12_VariablesNamingRules.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Variables can start from an alphabet or underscore or $ only
+    // Special Characters except _ and $ are not allowed
+    // Special Characters :
+    // ! @ # $ % ^ & * ( ) + - * / . [ ] { } : ; " ' < > , \ | _ ~ `
+    // Only $ and _ are allowed
+    // Commas or blanks are not allowed
+    // Some particular keywords are not allowed
+    // Keywords :
+    // double , float , int , char , break , continue , while , for , if , else , etc
+    return 0;
+}
