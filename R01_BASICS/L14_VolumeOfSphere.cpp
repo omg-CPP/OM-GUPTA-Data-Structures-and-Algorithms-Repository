db2413c132d4653c42fa28d607886bd24e396931
Om Gupta <omgbmw100@gmail.com>
2026-03-25 21:21:51 +0530

    Day 3: file-added

---
+++ R01_BASICS/L14_VolumeOfSphere.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double r = 4.8;
+    double pi = 3.1415;
+    double volume = (4*pi*r*r*r)/3;
+    cout<<"The Volume of Sphere with radius "<<r<<" is : "<<volume<<endl;
+    // The Volume of Sphere with radius 4.8 is : 463.233
+    return 0;
+}
