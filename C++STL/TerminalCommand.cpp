d418753346ec59d4cd8f679593f5b6b6cfed0c46
Om Gupta <omgbmw100@gmail.com>
2026-03-26 18:45:29 +0530

    Day 4: file-updated

---
+++ C++STL/TerminalCommand.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout<<"Enter a integer number: ";
+    cin>>x;
+    cout<<"The number you entered is: "<<x<<endl;
+}
+
+// .\  -> current directory
+// ..\ -> parent directory
+
+// -> cd C++STL
+// -> g++ TerminalCommand.cpp -o .\TerminalCommandOutputFile.exe
+// -> .\TerminalCommandOutputFile.exe
+
+//    Enter a integer number: 41
+//    The number you entered is: 41
