97a8c102539fd05d9047308a8da26ed6837f09cc
Om Gupta <omgbmw100@gmail.com>
2026-03-26 18:20:13 +0530

    Day 4: file-added

---
+++ R01_BASICS/L16_Operators.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Airthmetic Operators :
+    // + , - , * , / , % , ++ , --
+
+    // Relational Operators :
+    // == -> Is equal to -> 4 == 5 returns false
+    // != -> Not equal to -> 1 != 5 returns true
+    // > -> Greater than -> 1 > 5 returns false
+    // < -> Less than -> 2 < 5 returns true
+    // >= -> Greater than or equal to -> 4 >= 5 returns false
+    // <= -> Less than or equal to -> 1 <= 5 returns true
+
+    // Logical Operators :
+    // && -> Logical AND -> True only if both are true
+    // || -> Logical OR -> True if either is true
+    // ! -> Logical NOT -> True if expression is false
+
+    // Assignment Operators :
+    // = -> p = q
+    // += -> p += q
+    // -= -> p -= q
+    // *= -> p *= q
+    // /= -> p /= q
+    // %= -> p %= q
+
+    // Unary Operators :
+    // ++ -> Increment Operator -> ++p pre-increment -> p++ post-increment
+    // -- -> Decrement Operator -> --p pre-decrement -> p-- post-decrement
+
+    // Bitwise Operators :
+    // ~ -> Bitwise Complement
+    // << -> Left Shift
+    // >> -> Right Shift
+    // >>> -> Unsigned Right Shift
+    // & -> Bitwise AND
+    // ^ -> Bitwise Exclusive OR
+
+    // Precedency Of Operators : Highest To Lowest
+
+    // Unary -> ++ --
+    // * / %
+    // + -
+    // << >> >>> -> Shift
+    // Relational -> <= >= < >
+    // Equality -> == !=
+    // &
+    // ^
+    // |
+    // &&
+    // ||
+    // Conditional -> ?:
+    // Assignment -> = , += , -= , *= , /= , etc
+    return 0;
+}
