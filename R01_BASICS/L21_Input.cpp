e0fb9426f1304d0c48a3648c15f8d18c89b739eb
Om Gupta <omgbmw100@gmail.com>
2026-03-27 15:02:46 +0530

    Day 5: files-added

---
+++ R01_BASICS/L21_Input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter a number : ";
+    cin>>x;
+    cout<<"The number is : "<<x<<endl;
+    return 0;
+}
+
+// Enter a number : 5
+// The number is : 5
---
+++ R01_BASICS/L22_SquareOfNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number : ";
+    cin>>num;
+    int y = num*num;
+    cout<<"The Square of given number is : "<<y<<endl;
+    return 0;
+}
+
+// Enter a number : 7
+// The Square of given number is : 49  
