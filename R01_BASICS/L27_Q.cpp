eb7f113ca2b889e7cbcc8250a0241cc6979534c1
Om Gupta <omgbmw100@gmail.com>
2026-03-27 15:04:06 +0530

    Day 5: files-added

---
+++ R01_BASICS/L27_Q.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch = 'A';
+    int x = (int)ch;
+    cout<<x-64<<endl; // 1
+    return 0;
+}
---
+++ R01_BASICS/L28_HalfOfInteger.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Take an integer as input and print half of the number.
+    int x;
+    cout<<"Enter the number : ";
+    cin>>x;
+    float y = (float)x;
+    cout<<y/2<<endl; // 3.5
+    return 0;
+}
