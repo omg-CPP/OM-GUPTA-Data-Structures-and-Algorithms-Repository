32b03afc67e82f56097199438dc46034e9dc038d
Om Gupta <omgbmw100@gmail.com>
2026-03-27 15:03:22 +0530

    Day 5: files-added

---
+++ R01_BASICS/L23_SumOfTwoNumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout<<"Enter num1 : ";
+    cin>>num1;
+    cout<<"Enter num2 : ";
+    cin>>num2;
+    int sum = num1 + num2;
+    cout<<"The Sum of "<<num1<<" and "<<num2<<" is : "<<sum<<endl;
+    return 0;
+}
+
+// Enter num1 : 4
+// Enter num2 : 5
+// The Sum of 4 and 5 is : 9
