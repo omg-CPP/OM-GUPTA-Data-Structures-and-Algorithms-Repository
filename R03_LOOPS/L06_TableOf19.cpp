e4f84f003138c242343fb28cf8faf05b21f9dc01
Om Gupta <omgbmw100@gmail.com>
2026-03-31 02:05:02 +0530

    Day 9: file-added

---
+++ R03_LOOPS/L06_TableOf19.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i = 19 ; i <= 190 ; i += 19) {
+        cout<<i<<" ";
+    }
+    // 19 38 57 76 95 114 133 152 171 190 
+    cout<<endl;
+    return 0;
+}
