a7124828b5211e84cf1c0629fd64abb1649d9d0c
Om Gupta <omgbmw100@gmail.com>
2026-04-03 03:11:49 +0530

    Day 12: file-added

---
+++ R04_PATTERN-PRINTING/L28_CharPattern3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter no of rows : ";
+    cin>>n;
+    for(int i = 1 ; i <= n ; i++) {
+        int x = 65;
+        for(int j = 1 ; j <= n-i ; j++) {
+            cout<<"  ";
+        }
+        for(int j = 1 ; j <= i ; j++) {
+            cout<<(char)x<<" ";
+            x++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// Enter no of rows : 4
+//       A 
+//     A B 
+//   A B C 
+// A B C D 
