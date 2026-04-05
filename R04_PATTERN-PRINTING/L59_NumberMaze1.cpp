e6e77c9d6fdd3e851149f3807997eeb0b040c160
Om Gupta <omgbmw100@gmail.com>
2026-04-05 23:23:21 +0530

    Day 14: file-added

---
+++ R04_PATTERN-PRINTING/L59_NumberMaze1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter no of rows : ";
+    cin>>n;
+
+    for(int i = 1 ; i <= 2*n-1 ; i++) {
+        for(int j = 1 ; j <= 2*n-1 ; j++) {
+            int a = i;
+            if(i > n) {
+                a = 2*n-i;
+            }
+            int b = j;
+            if(j > n) {
+                b = 2*n-j;
+            }
+            cout<<min(a,b)<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// Enter no of rows : 4
+// 1 1 1 1 1 1 1 
+// 1 2 2 2 2 2 1 
+// 1 2 3 3 3 2 1 
+// 1 2 3 4 3 2 1 
+// 1 2 3 3 3 2 1 
+// 1 2 2 2 2 2 1 
+// 1 1 1 1 1 1 1 
