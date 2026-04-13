cc5d0f559dc758d8731b097a34b861798e515329
Om Gupta <omgbmw100@gmail.com>
2026-04-13 22:53:39 +0530

    Day 22- file-added

---
+++ R29_BIT-MANIPULATION/01-Learning-Bit-Manipulation/L09_CheckIfTheNumberIsAPowerOf2.cpp
@@ -0,0 +1,33 @@
+// Leetcode : 231 (EASY)
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <string.h>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <utility>
+#include <climits>
+#include <cfloat>
+#include <cmath>
+#include <math.h>
+#include <numeric>
+#include <algorithm>
+using namespace std;
+
+// T.C. = O(1)
+// S.C. = O(1)
+bool isPowerOfTwo(int n) {
+    return (n > 0) && ((n & n-1) == 0);
+}
+
+int main()
+{
+    return 0;
+}
