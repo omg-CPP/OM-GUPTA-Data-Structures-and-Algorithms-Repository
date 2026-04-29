728fc911d8273e5d76d65132898f353231623074
Om Gupta <omgbmw100@gmail.com>
2026-04-29 23:55:05 +0530

    Day 36: file-added

---
+++ R25_DYNAMIC-PROGRAMMING-DP/3 2D & 3D DP & DP on Grids/L01_UniquePathsI.cpp
@@ -0,0 +1,65 @@
+// Leetcode : 62
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// **************************** Memoization DP *********************************
+// T.C. = O(N*M)
+//       rec stack space   dp[][]
+// S.C. = O(path length) + O(N*M)
+//        O((m-1)+(n-1)) + O(N*M)
+int fun_Mem(int r, int c, vector<vector<int>>& dp) {
+    // base cases
+    if(r == 0 && c == 0) {
+        return 1;
+    }
+    if(r < 0 || c < 0) {
+        return 0;
+    }
+    // checking if the value is pre-computed
+    if(dp[r][c] != -1) {
+        return dp[r][c];
+    }
+    int up = fun_Mem(r-1,c,dp);
+    int left = fun_Mem(r,c-1,dp);
+    return dp[r][c] = up + left;
+}
+
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m,vector<int>(n,-1));
+    return fun_Mem(m-1,n-1,dp);
+}
+
+
+// **************************** Tabulation DP *********************************
+// T.C. = O(N*M)
+// S.C. = O(N*M)
+int uniquePaths_Tab(int m, int n) {
+    vector<vector<int>> dp(m,vector<int>(n,-1));
+    dp[0][0] = 1;
+    for(int i = 0 ; i < m ; i++) {
+        for(int j = 0 ; j < n ; j++) {
+            if(i == 0 && j == 0) {
+                dp[i][j] = 1;
+            }
+            else {
+                int up = 0;
+                int left = 0;
+                if(i > 0) {
+                    up = dp[i-1][j];
+                }
+                if(j > 0) {
+                    left = dp[i][j-1];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+int main()
+{
+    return 0;
+}
